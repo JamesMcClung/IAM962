6a53366b925ac6dbd82cbc26c2b385b3cc510f3a
James McClung <james@themcclungs.net>
2022-04-01 14:57:47 -0400

    main: initialize and print u

--- project1a/src/project1a.cc
+++ project1a/src/project1a.cc
@@ -1,15 +1,24 @@
 #include "project1a.hh"
 
+#include <cmath>
 #include <iostream>
 
 #include "linalg/fullmatrix.hh"
 
+using u_type = linalg::FullMatrix<nx, 1, real>;
+
+void set_to_initial_conditions(u_type &u) {
+    for (int i = 0; i < nx; i++) {
+        real x = i * len_x / nx + min_x;
+        u(i, 0) = std::sin(4 * M_PI * x);
+    }
+}
+
 int main() {
-    std::cout << "Hello, Prof. McHugh\n";
+    u_type u;
+    set_to_initial_conditions(u);
 
-    linalg::FullMatrix<3, 4, int> a(3);
-
-    std::cout << a;
+    std::cout << u << "\n";
 
     return 0;
 }
