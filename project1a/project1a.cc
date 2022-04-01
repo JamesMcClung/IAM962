ea4f5a64626e8fbba089b41c092290e7553b49c3
James McClung <james@themcclungs.net>
2022-03-31 20:46:40 -0400

    cmake: link to linalg

--- project1a/project1a.cc
+++ project1a/project1a.cc
@@ -1,6 +1,13 @@
 #include <iostream>
 
+#include "fullmatrix.hh"
+
 int main() {
     std::cout << "Hello, Prof. McHugh\n";
+
+    linalg::FullMatrix<3, 4, int> a(3);
+
+    std::cout << a;
+
     return 0;
 }
