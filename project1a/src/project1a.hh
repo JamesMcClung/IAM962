1bc37d58218c12947c2e2faf31c9e93fd9358f24
James McClung <james@themcclungs.net>
2022-04-01 20:08:57 -0400

    header: use real and add more consts

--- project1a/src/project1a.hh
+++ project1a/src/project1a.hh
@@ -12,16 +12,24 @@
 #define MIN_X_BC 0;  // boundary condition for u(min_x)
 #define MAX_X_BC 0;  // boundary condition for u(max_x)
 
+#define WRITE_EVERY 8;        // write u every <this many> time steps
+#define OUT_FILE "out.csv";   // path to output
+
 ////////////////////////////////////////////////////////////////////////
 // constants as variables for actual use in the code
+
+using real = double;
 
 static const double nu = NU;
 static const int nx = NX;
 static const int nt = NT;
-static const double min_x = MIN_X;
-static const double max_x = MAX_X;
-static const double len_x = max_x - min_x;
-static const double min_x_bc = MIN_X_BC;
-static const double max_x_bc = MAX_X_BC;
+static const real min_x = MIN_X;
+static const real max_x = MAX_X;
+static const real len_x = max_x - min_x;
+static const real dx = len_x / nx;
+static const real dt = dx * dx / nu;
+static const real min_x_bc = MIN_X_BC;
+static const real max_x_bc = MAX_X_BC;
 
-using real = double;
+static const int write_every = WRITE_EVERY;
+auto out_file = OUT_FILE;
