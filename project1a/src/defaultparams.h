12506c5ee543ce8c5637f65b298d4bbacd472281
James McClung <james@themcclungs.net>
2022-04-05 18:27:02 -0400

    params: use buildparams, remove config

---
+++ project1a/src/defaultparams.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#ifndef NU
+#define NU 1
+#endif
+
+#ifndef NX
+#define NX 128
+#endif
+
+#ifndef NT
+#define NT 1024
+#endif
+
+#ifndef MIN_X
+#define MIN_X 0
+#endif
+
+#ifndef MAX_X
+#define MAX_X 0
+#endif
+
+#ifndef DT
+#define USE_DT_OPT
+#define DT (dx * dx / nu)
+#endif
+
+#ifndef MIN_X_BC
+#define MIN_X_BC 0
+#endif
+
+#ifndef MAX_X_BC
+#define MAX_X_BC 0
+#endif
+
+#ifndef WRITE_EVERY
+#define WRITE_EVERY 8
+#endif
+
+#ifndef OUT_FILE
+#define OUT_FILE "out.csv"
+#endif
--- project1a/src/params.hh
+++ project1a/src/params.hh
@@ -1,24 +1,20 @@
 #pragma once
+
 #include "buildparams.h"
-#include "config.hh"
+#include "defaultparams.h"  // default params must be included after build params
 
 using real = double;  // type for values of u
 
-static constexpr real nu = P1A_NU;                          // diffusivity constant: u_t = nu * u_xx
-static constexpr int nx = P1A_NX;                           // number of spatial gridpoints
-static constexpr int nt = P1A_NT;                           // time steps to run for
-static constexpr real min_x = P1A_MIN_X;                    // lower bound for x domain
-static constexpr real max_x = P1A_MAX_X;                    // upper bound for x domain
-static constexpr real len_x = max_x - min_x;                // length of x domain
-static constexpr real dx = len_x / nx;                      // distance between spatial gridpoints
-#ifdef P1A_DT
-static constexpr real dt = P1A_DT;  // time between time steps
-#else
-#define USE_DT_OPT
-static constexpr real dt = dx * dx / nu;  // a convenient choice for dt
-#endif
-static constexpr real min_x_bc = P1A_MIN_X_BC;              // boundary condition for u(min_x)
-static constexpr real max_x_bc = P1A_MAX_X_BC;              // boundary condition for u(max_x)
+static constexpr real nu = NU;                // diffusivity constant: u_t = nu * u_xx
+static constexpr int nx = NX;                 // number of spatial gridpoints
+static constexpr int nt = NT;                 // time steps to run for
+static constexpr real min_x = MIN_X;          // lower bound for x domain
+static constexpr real max_x = MAX_X;          // upper bound for x domain
+static constexpr real len_x = max_x - min_x;  // length of x domain
+static constexpr real dx = len_x / nx;        // distance between spatial gridpoints
+static constexpr real dt = DT;                // time between time steps
+static constexpr real min_x_bc = MIN_X_BC;    // boundary condition for u(min_x)
+static constexpr real max_x_bc = MAX_X_BC;    // boundary condition for u(max_x)
 
-static constexpr int write_every = P1A_WRITE_EVERY;  // write u every <this many> time steps
-static constexpr auto out_file = P1A_OUT_FILE;       // path to output
+static constexpr int write_every = WRITE_EVERY;  // write u every <this many> time steps
+static constexpr auto out_file = OUT_FILE;       // path to output
