827ea28d5e5bee51e4d4e2b597b585ea989bc05a
James McClung <james@themcclungs.net>
2022-04-08 22:28:52 -0400

    scripts: simplify build_and_runs

--- project1c/scripts/build_and_run.sh
+++ project1c/scripts/build_and_run.sh
@@ -1,9 +1,4 @@
 #! /bin/bash
 
-# Example usage: ./scripts/build.sh NX=256 DT=.1
-
-args=$@
-cd ../build
-make project1c BUILDPARAMS="${args[@]}"
-cd ../project1c
-../build/project1c/project1c
+./scripts/build.sh $@
+./scripts/run.sh
