f2be77b947c286898afb6f698dffc62217dbbf1f
James McClung <james@themcclungs.net>
2022-04-11 21:39:55 -0400

    p1b/scripts,py: compare runs with varying params

---
+++ project1b/scripts/analyze.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo ab cfl dr > results.csv
+
+for AB in 1 2 3 4
+do
+    for CFL in $(seq .05 .05 .7)
+    do
+        ./scripts/build_and_run.sh which_ab=$AB cfl=$CFL
+        echo $AB $CFL $(python3 python/find_stability.py --bare out.csv) >> results.csv
+        # pause is necessary or else script can build and run while python
+        #   is still analyzing data, apparently
+        sleep .5
+    done
+done
