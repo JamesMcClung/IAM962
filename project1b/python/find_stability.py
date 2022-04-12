2c1017d64a6811f76611f21ac5fc4d506e465f9c
James McClung <james@themcclungs.net>
2022-04-11 20:45:27 -0400

    p1b/py: add bare option to find_stability

--- project1b/python/find_stability.py
+++ project1b/python/find_stability.py
@@ -6,15 +6,31 @@ from scipy import optimize
 from util import reader
 
 ########################################################################
+# Constants for flags/mods
+
+BARE_OUTPUT_FLAG = "--bare"
+
+BARE_OUTPUT_MODE = "bare"  # only print artificial damping rate
+FANCY_OUTPUT_MODE = "fancy"  # plot and add descriptions
+
+mode = FANCY_OUTPUT_MODE
+
+########################################################################
 # Check number of arguments
 
-if len(sys.argv) != 2:
+args = sys.argv[1:]
+
+if BARE_OUTPUT_FLAG in args:
+    mode = BARE_OUTPUT_MODE
+    args.remove(BARE_OUTPUT_FLAG)
+
+if len(args) != 1:
     sys.exit(f"Usage: {sys.argv[0]} path/to/data.csv")
 
 ########################################################################
 # Load data to plot
 
-path = sys.argv[1]
+path = args[0]
 
 u, x, t, c, which_AB, cfl = reader.read_uxtcwC(path)
 
@@ -27,10 +43,11 @@ amplitudes = (np.max(u, axis=1) - np.min(u, axis=1)) / 2
 # Determine when (if at all) the solution explodes
 
 explode_threshold = 10 * amplitudes[0]
-explode_idx = np.argmax(amplitudes > explode_threshold)
 
-if explode_idx:
-    print(f"-- explodes at t = {t[explode_idx]}")
+if mode != BARE_OUTPUT_MODE:
+    explode_idx = np.argmax(amplitudes > explode_threshold)
+    if explode_idx:
+        print(f"-- explodes at t = {t[explode_idx]}")
 
 ########################################################################
 # Determine the artificial damping rate, sigma
@@ -46,12 +63,21 @@ if explode_idx:
 
 damp_end_threshold = 4 * explode_threshold
 damp_end_idx = np.argmax(amplitudes > damp_end_threshold) or len(t)
-[_, sigma], _ = optimize.curve_fit(lambda t, A0, sigma: A0 * np.exp(-sigma * t), t[:damp_end_idx], amplitudes[:damp_end_idx], p0=[amplitudes[0], 0])
+[sigma], _ = optimize.curve_fit(
+    lambda t, sigma: amplitudes[0] * np.exp(-sigma * t),
+    t[:damp_end_idx],
+    amplitudes[:damp_end_idx],
+    p0=[0],
+)
 
-if sigma >0:
-    print(f"-- artificial damping rate = {sigma:.3}")
-else:
-    print(f"-- artificial growth rate >= {-sigma:.3}")
+if mode == FANCY_OUTPUT_MODE:
+    if sigma > 0:
+        print(f"-- artificial damping rate = {sigma:.3}")
+    else:
+        print(f"-- artificial growth rate >= {-sigma:.3}")
+elif mode == BARE_OUTPUT_MODE:
+    print(sigma)
+    sys.exit()
 
 ########################################################################
 # View the time evolution as an animated plot
