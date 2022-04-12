658b98bb034b5363893e9e36392366a1f2374770
James McClung <james@themcclungs.net>
2022-04-11 20:32:49 -0400

    p1b/py: option to save gif

--- project1b/python/plot.py
+++ project1b/python/plot.py
@@ -4,16 +4,33 @@ import matplotlib.animation as animation
 
 from util import reader
 
+
+########################################################################
+# Constants for flags/mods
+
+SAVE_PLOT_FLAG = "--save"
+
+SAVE_PLOT_MODE = "save"  # save plot as gif
+VIEW_PLOT_MODE = "view"  # view plot, but don't save
+
+mode = VIEW_PLOT_MODE
+
 ########################################################################
 # Check number of arguments
 
-if len(sys.argv) != 2:
+args = sys.argv[1:]
+
+if SAVE_PLOT_FLAG in args:
+    mode = SAVE_PLOT_MODE
+    args.remove(SAVE_PLOT_FLAG)
+
+if len(args) != 1:
     sys.exit(f"Usage: {sys.argv[0]} path/to/data.csv")
 
 ########################################################################
 # Load data to plot
 
-path = sys.argv[1]
+path = args[0]
 
 u, x, t, c, which_AB, cfl = reader.read_uxtcwC(path)
 
@@ -21,17 +38,26 @@ u, x, t, c, which_AB, cfl = reader.read_uxtcwC(path)
 # View the time evolution as an animated plot
 
 fig, ax = plt.subplots()
-line, = ax.plot(x, u[0])
+(line,) = ax.plot(x, u[0])
+
 
 def animate(i):
     ax.set_title(f"Wave Equation, AB{which_AB}, cfl={cfl}: $t={t[i]:.2f}$")
     line.set_ydata(u[i])
     return line, ax
 
+
 ani = animation.FuncAnimation(
-    fig, animate, interval=30, frames=len(t), repeat=True, repeat_delay=1000, blit=False)
+    fig, animate, interval=30, frames=len(t), repeat=True, repeat_delay=1000, blit=False
+)
 
 ax.set_xlabel("$x$")
 ax.set_ylabel("$u$")
 
-plt.show()
+if mode == VIEW_PLOT_MODE:
+    plt.show()
+else:
+    writergif = animation.PillowWriter(fps=30)
+    out_name = "out.gif"
+    ani.save(out_name, writer=writergif)
+    print(f"Saved to {out_name}")
