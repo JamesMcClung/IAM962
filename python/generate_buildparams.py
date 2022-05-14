ae34a341b3f3ece243883a66be183f5c94cb8494
James McClung <james@themcclungs.net>
2022-05-14 12:24:14 -0400

    py (fmt): format with black

--- python/generate_buildparams.py
+++ python/generate_buildparams.py
@@ -39,17 +39,19 @@ if not os.path.exists(dir):
 # Read old build params
 
 if os.path.isfile(path):
-    with open(path, 'r') as file:
+    with open(path, "r") as file:
         old_buildparams = file.read()
 else:
     old_buildparams = ""
-    
+
 ########################################################################
 # Utility
 
+
 def parse_defines(defines: str) -> dict:
     # make args case-insensitive with call to upper()
-    return {name.upper(): val or None for name, val in [define.split('=') for define in defines]}
+    return {name.upper(): val or None for name, val in [define.split("=") for define in defines]}
+
 
 def construct_c_line(name, val) -> str:
     if val:
@@ -57,14 +59,16 @@ def construct_c_line(name, val) -> str:
     else:
         return f"// use default {name}"
 
+
 def deconstruct_c_line(line: str) -> tuple:
     words = line.split(" ")
     if words[0] == "#define":
         # account for possible spaces in val, e.g. `#define foo "hello world"`
-        return words[1], ' '.join(words[2:])
+        return words[1], " ".join(words[2:])
     elif words[0].startswith("//"):
         # line such as `//#define NX` or `// use default NX`
         return words[-1], None
+
 
 ########################################################################
 # Generate new build params
@@ -72,20 +76,22 @@ def deconstruct_c_line(line: str) -> tuple:
 new_dict = parse_defines(defines)
 
 if mode == AMEND_MODE:
-    _, *old_c_lines = old_buildparams.split('\n')
+    _, *old_c_lines = old_buildparams.split("\n")
 
     old_dict = dict(map(deconstruct_c_line, old_c_lines))
     new_dict = old_dict | new_dict
 
 # sort so that permuting defines in command line doesn't force rebuild
-new_buildparams = "#pragma once\n" + "\n".join(sorted(construct_c_line(name, val) for name, val in new_dict.items()))
+new_buildparams = "#pragma once\n" + "\n".join(
+    sorted(construct_c_line(name, val) for name, val in new_dict.items())
+)
 
 ########################################################################
 # Update file if its contents would change
 
 if old_buildparams != new_buildparams:
     print(f"Updating: {path}")
-    with open(path, 'w') as file:
+    with open(path, "w") as file:
         file.write(new_buildparams)
 else:
     print(f"Already up-to-date: {path}")
