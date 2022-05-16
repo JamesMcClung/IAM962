f8a11834b1ec44d04393131dc65431bc8339bfa9
James McClung <james@themcclungs.net>
2022-05-16 11:24:16 -0400

    p2/sh (com): add comments explaining cursed line

--- project2a/scripts/test_speed.sh
+++ project2a/scripts/test_speed.sh
@@ -12,6 +12,8 @@ do
     TIME=0
     for _ in $(seq 1 $NRUNS)
     do
+        # bc does floating point math, and scale=3 sets how many decimal places to output
+        # sed extracts the wall time ("real") from the time command
         TIME=$(echo "scale=3; $TIME+$(/usr/bin/time ./scripts/run.sh 2>&1 | sed -r 's/.*([0-9]+\.[0-9]+).*real.*/\1/')" | bc)
     done
     TIME=$(echo "scale=3; $TIME/$NRUNS" | bc)
